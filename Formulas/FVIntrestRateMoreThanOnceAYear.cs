257e3d31b20a132ee0658de12df989ff25fac9ee
lilpaf <fokervt19@gmail.com>
2022-06-17 20:59:27 +0300

    Added FV for Compound Interest

---
+++ Formulas/FVIntrestRateMoreThanOnceAYear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Financial_Calculator.Abstract_Classes;
+using Financial_Calculator.RegexCheck;
+
+namespace Financial_Calculator.Formulas
+{
+    public class FVIntrestRateMoreThanOnceAYear : PVAndFVFormulas
+    {
+        
+        public override decimal Result()
+        {
+            return base.PresentValueOrFutureValue * (decimal)Math.Pow(1 + ((base.RateOfReturn / 100) / base.NumberOfCompoundingPeriods), base.NumberOfCompoundingPeriods * base.NumOfPeriods);
+        }
+    }
+}
