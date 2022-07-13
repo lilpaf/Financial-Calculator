e4d505df7f00bfd79615fe4f3306c6831f9e1281
lilpaf <fokervt19@gmail.com>
2022-07-13 21:32:21 +0300

    Added CompoundInterestIdentifier

---
+++ RegexCheck/CompoundInterestIdentifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financial_Calculator.RegexCheck
+{
+    public class CompoundInterestIdentifier
+    {
+        public static int Identifier(string text)
+        {
+            if (text.Contains("compounded monthly"))
+            {
+                return 12;
+            }
+            else if (text.Contains("compounded quarterly"))
+            {
+                return 4;
+            }
+
+            return 1; // by default compounded Annually
+        }
+          
+    }
+}
