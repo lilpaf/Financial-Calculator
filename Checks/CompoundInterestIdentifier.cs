acab828cd22f83b9253a798cda0cbc12ba6527c3
lilpaf <fokervt19@gmail.com>
2022-07-13 22:56:17 +0300

    Added semiannually compounding period

---
+++ Checks/CompoundInterestIdentifier.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Financial_Calculator.Checks
+{
+    public class CompoundInterestIdentifier
+    {
+        public static int Identifier(string text)
+        {
+            if (text.Contains("compounded monthly"))
+            {
+                return 12;
+            }
+            else if (text.Contains("compounded quarterly"))
+            {
+                return 4;
+            }
+            else if (text.Contains("compounded semiannually"))
+            {
+                return 2;
+            }
+
+            return 1; // by default compounded Annually
+        }
+          
+    }
+}
